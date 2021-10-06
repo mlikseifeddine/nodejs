d943013edb874fa343986b9ebd873e882c739f2e
mlikseifeddine <mliksaifeddine@outlook.com>
2021-10-06 12:44:30 +0200

    ricevimento di accessToken ,refreshToken,profile

--- index.js
+++ index.js
@@ -9,21 +9,33 @@ const app =express();
 
 
 //creare un istanza di google strategy di oauth20 
-passport.use(new GoogleStrategy({
+passport.use(
+    new GoogleStrategy({
     clientID:keys.googleClientID,
     clientSecret:keys.googleClientSecret,
     callbackURL:'/auth/google/callback'
-},()=>{
-console.log(accessToken);
-})
+},
+//informazioni che riceviamo 
+(accessToken,refreshToken,profile,done)=>{
+console.log('accessToken',accessToken);
+console.log('refreshToken',refreshToken);
+console.log('profile:',profile);
+}
+)
 );
 
 app.get('/auth/google',passport.authenticate('google',{
     scope:['profile','email']
-})
+}
+)
 );
+
+
+app.get('/auth/google/callback',passport.authenticate('google'));
 const PORT=process.env.PORT||5000;
 app.listen(PORT);
 
 
 // console.developers.google.com
+
+
