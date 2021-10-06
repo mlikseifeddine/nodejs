16706d779ea050074efd6c81652376de14cdc2ba
mlikseifeddine <mliksaifeddine@outlook.com>
2021-10-06 15:48:14 +0200

    prima del problema

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express=require('express');
+const mongoose=require('mongoose');
+const keys=require('./config/keys');
 require('./services/passport');
+mongoose.connect(keys.mongoURI);
 
 
 
@@ -11,3 +14,6 @@ const PORT=process.env.PORT||5000;
 app.listen(PORT);
 
 
+//M0GkdsUnuiOGJ1WS
+//saif
+//mongodb+srv://saif:M0GkdsUnuiOGJ1WS@emaily.k9sv0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
